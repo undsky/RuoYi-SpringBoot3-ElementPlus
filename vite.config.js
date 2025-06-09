04ae0a709e3599254f54fe3ab78a546baffc310d
jiangyanxi <jiangyanxi@live.com>
2025-06-09 10:44:09 +0800

    Duplicate key "build" in object literal

--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,6 @@ export default defineConfig(({ mode, command }) => {
     // 默认情况下，vite 会假设你的应用是被部署在一个域名的根路径上
     // 例如 https://www.ruoyi.vip/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.ruoyi.vip/admin/，则设置 baseUrl 为 /admin/。
     base: VITE_APP_ENV === 'production' ? '/admin/' : '/admin/',
-    build: {
-      copyPublicDir: false
-    },
     plugins: createVitePlugins(env, command === 'build'),
     resolve: {
       // https://cn.vitejs.dev/config/#resolve-alias
@@ -30,6 +27,7 @@ export default defineConfig(({ mode, command }) => {
     },
     // 打包配置
     build: {
+      copyPublicDir: false,
       // https://vite.dev/config/build-options.html
       sourcemap: command === 'build' ? false : 'inline',
       outDir: 'dist',
